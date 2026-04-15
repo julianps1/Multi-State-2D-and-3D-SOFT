0167c10554b7a9aba09f8ace7aa99625c1602db4
julianps1 <julesstetzler@gmail.com>
2026-04-15 11:51:44 -0400

    Added Test data for Ferretti model and cleaned up file paths

--- plot_all.sh
+++ plot_all.sh
@@ -3,13 +3,13 @@
 for file in output/snapshots/wf_*.dat
 do
     echo "Plotting $file"
-    gnuplot -c plot_density.gscript "$file"
+    gnuplot -c plotting/plot_density.gscript "$file"
 done
 
 echo "Plotting correlations and reaction probabilities"
-gnuplot plot_corr.gscript
-gnuplot plot_rxnprob.gscript
-gnuplot -c plot_ref.gscript "output/psiref.dat"
-gnuplot plot_pops.gscript
-gnuplot plot_pot.gscript
+gnuplot plotting/plot_corr.gscript
+gnuplot plotting/plot_rxnprob.gscript
+gnuplot -c plotting/plot_ref.gscript "output/psiref.dat"
+gnuplot plotting/plot_pops.gscript
+gnuplot plotting/plot_pot.gscript
 echo "Done :)"
